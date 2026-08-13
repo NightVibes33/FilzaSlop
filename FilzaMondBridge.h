ebba1687704bf0108cfd987fd66ff58f7d24354d
Zyn <bobbytatum999@gmail.com>
2026-08-13 12:13:52 -0500

    Add mond presentation bridge interface

---
+++ FilzaMondBridge.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+FOUNDATION_EXPORT void FilzaMondPresent(void);
+FOUNDATION_EXPORT void FilzaMondPresentFromController(UIViewController * _Nullable source);
+NS_ASSUME_NONNULL_END
